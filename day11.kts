89d281829a0a6ae7428d1fd7a608e65bafc84943
staffuser <119116574+staffuser@users.noreply.github.com>
2023-08-11 21:06:38 +0300

    Create day11.kts

---
+++ day11.kts
@@ -0,0 +1,10 @@
+val shapes: MutableList<String> = mutableListOf("triangle", "square", "circle")
+// Add "pentagon" to the list
+shapes.add("pentagon") 
+println(shapes)  
+// [triangle, square, circle, pentagon]
+
+// Remove the first "pentagon" from the list
+shapes.remove("pentagon") 
+println(shapes)  
+// [triangle, square, circle]
